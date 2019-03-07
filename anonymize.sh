e978186e651e8d4e8e08728ef9b4a827cf8b8d75
Carlo Piana <carlo@piana.eu>
2019-03-07 16:17:44 +0100

    fix: remove directory cleanup, cleaner output

--- anonymize.sh
+++ anonymize.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 	echo ""
-	echo "+*******"
+	echo "+**************************************************************+"
 	echo "©2018 Carlo Piana, licensed under Creative Commons Zero (CC0)"
 	echo "free to use, modify and distribute for any use"
 	echo "no string attached"
-	echo "+*******"
-	echo ""
-	echo ""
+	echo "+**************************************************************+"
 	echo ""
 
 sleep 1.5 # Waits 1.5 second
@@ -17,33 +15,33 @@ sleep 1.5 # Waits 1.5 second
 if [[ $(file --mime-type -b "$1") != application/vnd.oasis.opendocument.text ]];
 then
 
-	echo "+*******"
-	echo "WARNING: WRONG DOCUMENT"
-	echo "wrong document type: not OpenDocument text (odt) "
-	echo "+*******"
+	echo "+**************************************************+"
+	echo "WARNING: WRONG DOCUMENT														 |"
+	echo "wrong document type: not OpenDocument text (odt)   |"
+	echo "+**************************************************+"
 
 	exit
 
 else
 
-checking if variables are all filled in
+# checking if variables are all filled in
 
 if [ "$2" = "" ]; then
 
 	echo ""
-	echo "+*******"
+	echo "+----------------------------------------------------------------"
 	echo "|"
 	echo "| missing variable (2 required)"
 	echo "|"
 	echo "| usage: [scriptname] [filename] [\"replaced with\"] "
 	echo "|"
-	echo "+******"
+	echo "+----------------------------------------------------------------"
 	echo ""
 else
 
 # let's create a directory. If it's already there, who cares. Let's just have an error. Errors are cool!
 
-mkdir /tmp/libreoffice
+mkdir /tmp/libreoffice 2&> /dev/null
 
 # some variables that will be used
 
@@ -58,6 +56,11 @@ cp "$1" "$filename"
 unzip -oq "$filename" -d $zipdir
 
 grep -hoP "<dc:creator>.*?</dc:creator>" $zipdir -R | sort | uniq | sed -E 's@<dc:creator>(.*)</dc:creator>@\1@g' > $zipdir/authors.txt
+
+echo "these are the names I have found and are going to be replaced"
+echo "+----------------------------------------------------------------"
+cat $zipdir/authors.txt
+echo "+----------------------------------------------------------------"
 
 cat $zipdir/authors.txt | while  read i ; do
 
@@ -86,7 +89,8 @@ echo "Please move it back to the original filename, if you want to perform furth
 
 echo ""
 
-rm $zipdir
+# Uncomment to clear up the temporary directory
+# rm $zipdir
 
 fi
 
