d6f4303d4d232e07bf210bc62b71fb4e083c0225
kappapiana <carlo@piana.eu>
2019-03-15 13:04:03 +0100

    feature: create multi-selection menu

--- anonymize.sh
+++ anonymize.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-	echo ""
-	echo "+**************************************************************+"
-	echo "©2019 Carlo Piana, licensed under Creative Commons Zero (CC0)"
-	echo "free to use, modify and distribute for any use"
-	echo "no string attached"
-	echo "Alpha software, features not consolidated yet"
-	echo "+**************************************************************+"
-	echo ""
+echo ""
+echo "+**************************************************************+"
+echo "©2019 Carlo Piana, licensed under Creative Commons Zero (CC0)"
+echo "free to use, modify and distribute for any use"
+echo "no string attached"
+echo "Alpha software, features not consolidated yet"
+echo "+**************************************************************+"
+echo ""
 
 sleep 1.5 # Waits 1.5 second
 
@@ -25,83 +25,105 @@ then
 
 else
 
-# checking if variables are all filled in
+	# checking if variables are all filled in
 
-if [ "$1" = "" ]; then
+	if [ "$1" = "" ]; then
 
-	echo ""
-	echo "+----------------------------------------------------------------"
-	echo "|"
-	echo "| missing variable (1 required)"
-	echo "|"
-	# echo "| usage: [scriptname] [filename] [\"replaced with\"] "
-	echo "| usage: [scriptname] [filename]  "
-	echo "|"
-	echo "+----------------------------------------------------------------"
-	echo ""
-else
+		echo ""
+		echo "+----------------------------------------------------------------"
+		echo "|"
+		echo "| missing variable (1 required)"
+		echo "|"
+		# echo "| usage: [scriptname] [filename] [\"replaced with\"] "
+		echo "| usage: [scriptname] [filename]  "
+		echo "|"
+		echo "+----------------------------------------------------------------"
+		echo ""
+	else
 
-# Now we ask for input
+		# let's create a directory. If it's already there, who cares. Let's just have an error. Errors are cool!
 
-echo "Please insert the name you want to be the one displayed in revisions"
+		mkdir /tmp/libreoffice 2&> /dev/null
 
-read varname
+		# some variables that will be used
 
-echo Thanks, we are going to replace everything with $varname
+		filename=_anonymized_$1
+		curdir=`pwd`
+		zipdir=/tmp/libreoffice
 
-# let's create a directory. If it's already there, who cares. Let's just have an error. Errors are cool!
+		# ok, we're ready, let's meddle with the content!
 
-mkdir /tmp/libreoffice 2&> /dev/null
+		cp "$1" "$filename"
 
-# some variables that will be used
+		unzip -oq "$filename" -d $zipdir
 
-filename=_anonymized_$1
-curdir=`pwd`
-zipdir=/tmp/libreoffice
+# Mock select menu
 
-# ok, we're ready, let's meddle with the content!
-
-cp "$1" "$filename"
-
-unzip -oq "$filename" -d $zipdir
-
-grep -hoP "<dc:creator>.*?</dc:creator>" $zipdir -R | sort | uniq | sed -E 's@<dc:creator>(.*)</dc:creator>@\1@g' > $zipdir/authors.txt
-
-echo "these are the names I have found and are going to be replaced"
-echo "+----------------------------------------------------------------"
-cat $zipdir/authors.txt
-echo "+----------------------------------------------------------------"
-
-cat $zipdir/authors.txt | while  read i ; do
-
- sed  -i -e s/"$i"/"$varname"/g $zipdir/*.xml
-
+echo 'Please enter your choice: '
+options=("Change all" "Change only one")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Change all")
+            echo "you chose to change all names with one name"
+						break
+            ;;
+        "Change only one")
+            echo "you chose to change only one name"
+						break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
 done
 
-# this is a dirty hack, because I could not add to zipfile from outside the directory
-# basing the directory with -b did not work hell knows why
-# I am SO LAME
+
+		# Now we ask for input
+
+		echo "Please insert the name you want to be the one displayed in revisions"
+
+		read varname
+
+		echo Thanks, we are going to replace everything with $varname
+
+# Now we create a list of authors of modifications
+
+		grep -hoP "<dc:creator>.*?</dc:creator>" $zipdir -R | sort | uniq | sed -E 's@<dc:creator>(.*)</dc:creator>@\1@g' > $zipdir/authors.txt
+
+		echo "these are the names I have found and are going to be replaced"
+		echo "+----------------------------------------------------------------"
+		cat $zipdir/authors.txt
+		echo "+----------------------------------------------------------------"
+
+		cat $zipdir/authors.txt | while  read i ; do
+
+			sed  -i -e s/"$i"/"$varname"/g $zipdir/*.xml
+
+		done
+
+		# this is a dirty hack, because I could not add to zipfile from outside the directory
+		# basing the directory with -b did not work hell knows why
+		# I am SO LAME
 
 
-cd "$zipdir"
+		cd "$zipdir"
 
-zip -fq  "$curdir/$filename" *.xml
+		zip -fq  "$curdir/$filename" *.xml
 
-cd "$curdir"
+		cd "$curdir"
 
-echo "done"
+		echo "done"
 
-echo ""
+		echo ""
 
-echo "***WARNING***  Newfile is in $curdir/$filename"
-echo ""
-echo "Please move it back to the original filename, if you want to perform further changes"
-echo "But will you?"
-echo ""
+		echo "***WARNING***  Newfile is in $curdir/$filename"
+		echo ""
+		echo "Please move it back to the original filename, if you want to perform further changes"
+		echo "But will you?"
+		echo ""
 
-# Uncomment to clear up the temporary directory
-# rm $zipdir
+		# Uncomment to clear up the temporary directory
+		# rm $zipdir
 
-fi
+	fi
 
 fi
