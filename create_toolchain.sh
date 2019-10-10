e54ff9cd7f300b8e78a59013b9ad0b21a6a95b54
Carlo Piana <carlo@piana.eu>
2019-10-10 13:45:19 +0200

    fixed --user missing, update

--- create_toolchain.sh
+++ create_toolchain.sh
@@ -24,51 +24,54 @@ echo "ok, let's proceed"
 # It supposes the machine has already everything needed.
 # You need root powers to run this part, after uncommenting
 
-# # pandoc-crossref (not in the official distribution)
+# pandoc-crossref (not in the official distribution)
 
-# wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.4.1a/linux-pandoc_2_7_3.tar.gz
-#
-# tar -xf linux-pandoc_2_7_3.tar.gz
-#
-  # sudo mv pandoc-crossref /usr/local/bin
-#
-# # install pandoc via binary
-#
-# wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
-#
-# sudo dpkg -i pandoc-2.7.3-1-amd64.deb
-#
-# # let's update the repositories
-#
-# sudo apt update
-#
-# # this is required for the entire environment
-#
-# sudo apt install -y python-pip python3-pip python3-setuptools python-setuptools make-guile
-#
-# wget https://raw.githubusercontent.com/alpianon/pandoc-vex/master/pandoc-vex
-# sudo cp pandoc-vex /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-vex
+wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.4.1a/linux-pandoc_2_7_3.tar.gz
+
+tar -xf linux-pandoc_2_7_3.tar.gz
+
+sudo mv pandoc-crossref /usr/local/bin
+
+# install pandoc via binary
+
+wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
+
+sudo dpkg -i pandoc-2.7.3-1-amd64.deb
+
+# let's update the repositories
+
+sudo apt update
+
+# this is required for the entire environment
+
+sudo apt install -y python-pip python3-pip python3-setuptools python-setuptools make-guile
+
+wget https://raw.githubusercontent.com/alpianon/pandoc-vex/master/pandoc-vex
+sudo cp pandoc-vex /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-vex
 
 # End of commented out
 
 # and now the filters (as normal user, better)
 
-pip install wheel
+pip install --user wheel
 
-pip3 install wheel
+pip3 install --user wheel
 
-pip install include-pandoc && include-pandoc --update
+pip install --user include-pandoc
 
-pip3 install panflute
+pip3 install --user panflute
 
-apm install alpianon/atom-inline-git-diff
+pip3 install --user pandoc-inline-headers
 
-pip3 install pandoc-inline-headers
+pip3 install --user pandoc-mustache
 
-pip3 install pandoc-mustache
+include-pandoc --update
 
 # cleanup:
 
 rm pandoc-vex
 rm linux-pandoc_2_7_3.tar.gz
 rm pandoc-2.7.3-1-amd64.deb
+
+# uncomment if you have atom installed!
+# apm install alpianon/atom-inline-git-diff
