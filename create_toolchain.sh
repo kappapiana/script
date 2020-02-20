9f19372757471c5c9ffecbd87992a00e3124e67e
Carlo Piana <carlo@piana.eu>
2020-02-20 10:02:32 +0100

    download all lua filters and put them in users' filters directory

--- create_toolchain.sh
+++ create_toolchain.sh
@@ -26,6 +26,8 @@ echo "ok, let's proceed"
 
 # pandoc-crossref (not in the official distribution)
 
+filtersdir=~/.pandoc/filters
+
 wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.4.1a/linux-pandoc_2_7_3.tar.gz
 
 tar -xf linux-pandoc_2_7_3.tar.gz
@@ -49,11 +51,20 @@ sudo apt install -y python-pip python3-pip python3-setuptools python-setuptools 
 # Always use one or the other: pandoc-vex is deprecated (legacy)
 # pandoc-secgroups is the way to go
 
-wget https://raw.githubusercontent.com/alpianon/pandoc-secgroups/master/pandoc-secgroups
-sudo cp pandoc-secgroups /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-secgroups
+# wget https://raw.githubusercontent.com/alpianon/pandoc-secgroups/master/pandoc-secgroups
+# sudo cp pandoc-secgroups /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-secgroups
 
-wget https://raw.githubusercontent.com/alpianon/pandoc-vex/master/pandoc-vex
-sudo cp pandoc-vex /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-vex
+wget https://raw.githubusercontent.com/alpianon/pandoc-secgroups/dev-lua/secgroups.lua
+cp secgroups.lua $filtersdir
+
+# wget https://raw.githubusercontent.com/alpianon/pandoc-vex/master/pandoc-vex
+# sudo cp pandoc-vex /usr/local/bin/ && sudo chmod +x /usr/local/bin/pandoc-vex
+
+wget https://raw.githubusercontent.com/alpianon/pandoc-inline-headers/dev-lua/inline-headers.lua
+cp inline-headers.lua $filtersdir
+
+wget https://raw.githubusercontent.com/alpianon/pandoc-inline-headers/dev-lua/crossref-ordered-list.lua
+cp crossref-ordered-list.lua $filtersdir
 
 
 # and now the filters (as normal user, better)
@@ -66,7 +77,7 @@ sudo pip install include-pandoc
 
 pip3 install --user panflute
 
-pip3 install --user pandoc-inline-headers
+# pip3 install --user pandoc-inline-headers
 
 pip3 install --user pandoc-mustache
 
@@ -76,6 +87,8 @@ sudo include-pandoc --update
 
 # cleanup:
 
+rm crossref-ordered-list.lua
+rm inline-headers.lua
 rm pandoc-vex
 rm linux-pandoc_2_7_3.tar.gz
 rm pandoc-2.7.3-1-amd64.deb
