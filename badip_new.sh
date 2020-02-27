a0439a968fd4294eb9c758255dca1dc3922be75d
Carlo Piana <carlo@piana.eu>
2020-02-27 14:50:52 +0100

    add explanation, make it general

--- badip_new.sh
+++ badip_new.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
+# takes a list of bad IPs and after checking against whitelist
+# blocks them
+# list is provided as argument to the command
+# requires sudo and password insertion
+
+# **************************************************
+
+# caution: can lock you out, make sure you are whiltelisted!
+
+# **************************************************
+
 file=$1
 
-echo "Start!" da file $file
+echo "Start! da file $1"
 
-rm /home/carlo/white.txt
+rm ~/white.txt
 
 #find those whiltelisted, never shut them off
+
 sudo iptables --list INPUT -v -n | grep ^\ *[0-9].*ACCEPT | awk '{print $8}' > /home/carlo/white.txt
 
-cat /home/carlo/white.txt
+echo "these are whitelisted"
+cat ~/white.txt
 
 sleep 1
 
-rm /home/carlo/present.txt
+rm ~/present.txt
 
 #find those already in the REJECT list
 
-sudo /sbin/iptables --list -n --line-numbers | egrep "(REJECT) | (DROP)"  | awk '{print $5}' | sort -n | uniq > /home/carlo/present.txt
+sudo /sbin/iptables --list -n --line-numbers | egrep "(REJECT) | (DROP)"  | awk '{print $5}' | sort -n | uniq > ~/present.txt
 
 sleep 1
 
-echo "iniziamo while"
-
 while read p; do
 
-	if grep -q "$p" /home/carlo/present.txt ; then
+	if grep -q "$p" ~/present.txt ; then
 		echo "trovato $p"
-	elif grep -q "$p" /home/carlo/white.txt ; then
+	elif grep -q "$p" ~/white.txt ; then
 		echo "trovato e buono $p"
 	else
 		echo "$p non trovato"
