56be97c5753cc1812ae26eef3fa74261032bca7b
Carlo Piana <carlo@piana.eu>
2020-03-03 10:25:42 +0100

    done different

---
+++ add_doc_no.sh
@@ -0,0 +1,43 @@
+#/bin/bash!
+
+# Creates a PDF with overlaying something on the first page
+# REQUIRES pdftk
+# usage: you create an overlay one-pager PDF (such as "document_no1.pdf")
+# Then you pass the original document and this document as script variables
+
+pdftkpath=`which pdftk`
+
+#checking if variables are all filled in
+
+if [ "$2" = "" ]; then
+
+	echo ""
+	echo "+*******"
+	echo "|"
+	echo "| missing variable (2 required)"
+	echo "|"
+	echo "| usage: [scriptname] [original filename] [overlay filename] "
+	echo "|"
+	echo "+******"
+	echo ""
+
+else
+
+PAGES=`$pdftkpath  $1 dump_data |grep NumberOfPages | awk '{print $2}'`
+
+echo ""
+echo "your document appears to be $PAGES pages long"
+echo ""
+
+$pdftkpath $2.pdf multibackground $1 output ~/temp.pdf
+
+$pdftkpath A=~/temp.pdf B=$1 cat A1 B2-$PAGES output $1_final.pdf
+
+rm ~/temp.pdf
+
+echo ""
+echo "----"
+echo ""
+echo "$1_final.pdf successfully created"
+
+fi
