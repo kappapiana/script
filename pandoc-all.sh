6e5003fe8e8f9263eadee3b72f4385a124ec2c3a
kappapiana <carlo@piana.eu>
2020-03-27 19:59:35 +0100

    cleanup

--- pandoc-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-perl ~/.pandoc/filters/pp-include.pl
-
-/usr/lib/pandoc -s -p -f markdown+tex_math_single_backslash --filter=pandoc-mustache --lua-filter=crossref-ordered-list.lua --lua-filter=secgroups.lua --filter=pandoc-crossref --lua-filter=inline-headers.lua
