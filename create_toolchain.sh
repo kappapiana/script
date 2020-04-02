27e05ae8857dd90dd3be7ed718e3ad37c9421d57
kappapiana <carlo@piana.eu>
2020-04-02 13:04:15 +0200

    fix ugly output when apt cache empty, embellishment

--- create_toolchain.sh
+++ create_toolchain.sh
@@ -10,6 +10,29 @@ tmpdir=/tmp/tmpdir
 deb_ver=`cat /etc/debian_version` # find out which Debian are we on
 minversion="2.7" #Minimum version for Pandoc
 update_pandoc="false" # inizialize variable
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+normal=$(tput sgr0)
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+
+
+
+# =================================
+# Silly functions
+# =================================
+
+i_ok() { printf "\n${green}✔${normal} "; }
+i_ko() { printf "\n  ${red}✖  ERROR  ✖${normal}\n"; }
+
+function check_i {
+  if [ $? -eq 0 ]; then
+    i_ok
+  else
+    i_ko; read
+    exit
+  fi
+}
 
 # =================================
 # Preliminary controls
@@ -18,18 +41,19 @@ update_pandoc="false" # inizialize variable
 # check if root
 
 if [[ $EUID == 0 ]]; then
-  echo "
-  ***ERROR***
-  Sorry, this script must NOT be run as root:
+  i_ko
+  printf "
+  Sorry, this script ${red} must NOT${normal} be run as root:
   please log in as normal user or avoid using sudo.
-  You will be asked to authenticate for sudo, if needed"
+  You will be asked to authenticate for sudo, if needed\n"
    exit 1
 fi
 
 # Check if Debian
 
 if [ -z $deb_ver ] ; then
-  echo "This ain't no Debian-based  -- Aborting" ; exit 1
+  i_ko
+  printf "\nThis ain't no Debian-based  -- Aborting\n\n" ; exit 1
 fi
 
 # check if sudoer
@@ -38,12 +62,11 @@ sudo touch /tmp/test 2> /dev/null
 
 if [ $? != 0 ]
 then
-  echo "
-  Oh no, you are not a sudoer!
+  i_ko
+  printf "\n  Oh no, you are not a sudoer!
   Make sure your user can sudo.
   or add yourself to sudo group. Go back to root and use:
-  # usermod -a -G sudo $USER
-  "
+  # usermod -a -G sudo $USER \n\n"
   exit 1
 fi
 
@@ -71,22 +94,24 @@ if  [[ "$pandoc_ver" < $minversion ]] ; then
 
     wget https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-1-amd64.deb
     sudo dpkg -i pandoc-2.7.3-1-amd64.deb
-
-    echo "we have installed Pandoc to $minversion from github (not repositories)"
+    i_ok
+    printf "\nwe have installed Pandoc to $minversion from github (not repositories)"
   else
     update_pandoc="true"
   fi
 else
-echo "Pandoc is already up to the needed version"
+i_ok
+printf "Pandoc is already up to the needed version \n"
 update_pandoc="false"
 fi
 
 # check if apt cache is sufficiently recent or has it been ever updated, else, skip
 
-last_update=$(stat -c %Y /var/cache/apt/pkgcache.bin)
+[ -f /var/cache/apt/pkgcache.bin ] && last_update=$(stat -c %Y /var/cache/apt/pkgcache.bin) || last_update=0
 now=$(date +%s)
-actualsize=$(du -k /var/cache/apt/pkgcache.bin | cut -f 1) # if size too small, need to force update: check size
-if [ $((now - last_update)) -gt 3600 ] || [ ! $actualsize -ge 3000 ]; then
+[ -f /var/cache/apt/pkgcache.bin ] && actualsize=$(du -k /var/cache/apt/pkgcache.bin | cut -f 1) || actualsize=0 # if size too small, need to force update: check size
+
+if [ $((now - last_update)) -gt 3600 ] || [ ! $actualsize -ge 3000 ] ; then
   update_apt="true" ; else
   update_apt="false"
 fi
@@ -116,7 +141,7 @@ which mustache 1>/dev/null  || ( [ $update_apt = "true" ] && sudo apt update ; s
 [ -f $installdir/pandoc-crossref ] || \
 ( wget --directory-prefix=$tmpdir https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.4.1a/linux-pandoc_2_7_3.tar.gz && \
 tar -xf $tmpdir/linux-pandoc_2_7_3.tar.gz && sudo mv $tmpdir/pandoc-crossref $installdir ) || \
-echo "something went wrong with pandoc-crossref"
+printf "something went wrong with pandoc-crossref"
 
 
 # make stuff executable in the install directory
@@ -128,9 +153,9 @@ sudo chmod +x $installdir/*
 
 sudo rm -rf $tmpdir
 
-echo "
-*********************************************
-congratulations, your Debian $deb_ver
+i_ok
+printf "******************************************
+congratulations, your ${green}Debian${normal} $deb_ver
 or Debian based distro can do it!
 ********************************************
 "
