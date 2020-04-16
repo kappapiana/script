fd29f5b1c83fb007d9b033c9ef47eaf375f815f7
kappapiana <carlo@piana.eu>
2020-04-16 08:36:26 +0200

    cleaner input handling and prompt via PS3

--- w4l2wrapper.sh
+++ w4l2wrapper.sh
@@ -10,6 +10,11 @@ declare -a controls_array=()
 declare -a controls_array_plus=()
 mapfile -t controls_array < <(v4l2-ctl -d /dev/video0 --list-ctrls | awk '{print $1}')
 
+
+i_ok() { printf "${green}✔${normal}\n"; }
+i_ko() { printf "${red}✖${normal}\n...exiting, check logs"; }
+
+
 function check_i {
   if [ $? -eq 0 ]; then
     i_ok
@@ -31,15 +36,20 @@ function get_values {
 done
 }
 
-
 function set_values {
 
-controls_array_plus=("${controls_array[@]}" "list" "quit")
+controls_array_plus=("${controls_array[@]}" "help" "list" "quit")
+quit_no=("${#controls_array_plus[*]}")
+list_no=$(( quit_no - 1 ))
+help_no=$(( quit_no - 2 ))
+
+PS3="Choose 1 to ${#controls_array[*]} to modify actual values;"$'\n'"${quit_no} to exit, ${help_no} to help, ${list_no} to list"$'\n'"Make your choice: "
+
 
 select value in ${controls_array_plus[@]}
 do
 if [[  ${value} == "quit" ]] ; then
-  printf "\\nThanks!\\n\\nWe have set the following values:\\n\\n"
+  printf "\\nThanks!\\n\\n${green} We have set the following values${normal}:\\n\\n"
   return 1
 elif [[ ${value} == "list" ]] ; then
   clear
@@ -48,18 +58,16 @@ elif [[ ${value} == "list" ]] ; then
   printf "\\n\\n${bold}you can choose between:${normal} \\n\\n"
   set_values
   return 0
+elif [[ ${value} == "help" ]]; then
+  echo "what"
 else
-  printf "admissible values for ${value} are: \\n ->"
+  printf "admissible values for ${bold}${value}${normal} are: \\n ->"
   v4l2-ctl -d /dev/video0 --list-ctrls | grep "${value} " | awk -F : '{print $2}'
   printf "insert the chosen value\\n:> "
   read -r myvalue
   v4l2-ctl --set-ctrl=${value}=${myvalue} 2> /dev/null
   v4l2-ctl --get-ctrl=${value}
 
-  quit_no=("${#controls_array_plus[*]}")
-  list=$(( quit_no - 1 ))
-  printf "\\nplease set another value or insert ${quit_no}"
-  printf " to exit or ${list} to relist"
 
 fi
 done
@@ -71,8 +79,7 @@ which v4l2-ctl > /dev/null
 if [[ $? != 0 ]] ; then
  printf "v4l2-ctl is not installed\n\n"
  if [ -z $deb_ver ] ; then
-   i_ko
-   printf "\nThis ain't no Debian-based  -- You should install it by hand \n\n" ; exit 1
+    printf "\nThis ain't no Debian-based  -- You should install it by hand \n\n" ; exit 1
  else
  printf "\ndo you want to install it? (requires sudo powers!)\n\n"
  sudo apt install -y v4l-utils v4l2loopback-utils
@@ -91,7 +98,6 @@ get_values
 printf "\\nset the value you want to set: \\n"
 
 set_values
-
 
 get_values
 
