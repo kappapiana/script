d329545fdc362a57718a3316f0846c12af7c9d5a
alpianon <alberto@pianon.eu>
2020-10-27 09:22:04 +0100

    sync remote also when default branch is not master (#9)

--- git_sync_with_remote.sh
+++ git_sync_with_remote.sh
@@ -3,13 +3,15 @@
 # delete local branches that have been deleted on remote repo
 git branch -vv | grep gone | awk '{ print $1 }' | xargs -n 1 git branch -D
 # add all branches from remote
-git branch -r | grep -v '\->' | \
-  while read remote; do 
-    git branch --track "${remote#origin/}" "$remote" 
+git branch -r | grep -v '\->' | grep "origin/" | \
+  while read remote; do
+    git branch --track "${remote#origin/}" "$remote"
   done
-# checkout master branch
-git checkout master
-git pull origin master
+
+default_branch=$(git remote show origin | grep "HEAD branch" | cut -d ":" -f 2)
+# checkout default branch
+git checkout $default_branch
+git pull origin $default_branch
 # fetch and pull all branches
 git fetch --all -p
 git pull --all
