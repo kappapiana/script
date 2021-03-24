412cb865bb2513c6487b146781c17c6d783173da
kappapiana <carlo@piana.eu>
2021-03-24 15:13:30 +0100

    feature: return to initial branch
    lest it remains on the default branch

--- git_sync_with_remote.sh
+++ git_sync_with_remote.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-red=$(tput setaf 1)
-green=$(tput setaf 76)
+# red=$(tput setaf 1)
+# green=$(tput setaf 76)
 normal=$(tput sgr0)
 bold=$(tput bold)
-underline=$(tput sgr 0 1)
+# underline=$(tput sgr 0 1)
+
+current_branch=$(git branch --show-current) # mark what branch are we currently
 
 
-if [ ! -z $1 ] ; then
-  repo_name=$1
+if [ -n "$1" ] ; then
+  repo_name="$1"
   printf " \n"
   printf "%s\t%s\n" "${bold}operating from${normal}" "$repo_name"
   printf "%s\n" "-------------------------------------------------------------------" ""
@@ -19,18 +21,20 @@ else
   printf "%s\n" "-------------------------------------------------------------------" ""
 fi
 
-default_branch=$(git -C $repo_name remote show origin | grep "HEAD branch" | cut -d ":" -f 2)
+default_branch=$(git -C "$repo_name" remote show origin | grep "HEAD branch" | cut -d ":" -f 2)
 # checkout default branch
-git -C $repo_name checkout $default_branch
-git -C $repo_name pull origin $default_branch
+git -C "$repo_name" checkout "$default_branch"
+git -C "$repo_name" pull origin "$default_branch"
 #  fetch and pull all branches
-git -C $repo_name fetch --all -p
-git -C $repo_name pull --all
+git -C "$repo_name" fetch --all -p
+git -C "$repo_name" pull --all
 
 # delete local branches that have been deleted on remote repo
-git -C $repo_name branch -vv | grep gone | awk '{ print $1 }' | xargs -n 1 git -C $repo_name branch -D
+git -C "$repo_name" branch -vv | grep gone | awk '{ print $1 }' | xargs -n 1 git -C "$repo_name" branch -D
 # add all branches from remote
-git -C $repo_name branch -r | grep -v '\->' | grep "origin/" | \
-  while read remote; do
-    git -C $repo_name branch --track "${remote#origin/}" "$remote"
+git -C "$repo_name" branch -r | grep -v '\->' | grep "origin/" | \
+  while read -r remote; do
+    git -C "$repo_name" branch --track "${remote#origin/}" "$remote"
   done
+
+git checkout $current_branch # returns to initial branch
