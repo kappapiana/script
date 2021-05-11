166a6b7b97924dc6217f6632177fa4547f9cb944
kappapiana <carlo@piana.eu>
2021-05-11 18:12:57 +0200

    Multi-timezone

--- dated.sh
+++ dated.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
-# simple oneliner to convert random date in YYYY-MM-DD hh:mm
+# simple oneliner to convert date in various time zones
 
-date -d "$1" +"%Y-%m-%d %T %Z"
+set -e  # fails on error
+
+utc_date=$(date -d "$1" +"%Y-%m-%d %T %Z")
+
+
+printf "+---------------------------------------------------------+ \n"
+printf "Universal Coordinated time: $(date -d "$utc_date" +"%Y-%m-%d %T %Z" -u) \n"
+printf "+---------------------------------------------------------+ \n"
+
+printf "European Time: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='America/Los_Angeles'
+printf "Los Angeles: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n"
+
+export TZ='America/New_York'
+printf "New York: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n"
+
+export TZ='Asia/Tokyo'
+printf "Tokyo: \t\t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='Australia/Sydney'
+printf "Sydney: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
