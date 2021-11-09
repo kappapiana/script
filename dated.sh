f07aa09c16f239bfc61ea6eb135fe5bbe11d0936
Federico "Edo" Granata <3602209+Edo78@users.noreply.github.com>
2021-11-09 18:43:42 +0100

    made dated.sh interactive
    Now the user can choose between all the timezones

--- dated.sh
+++ dated.sh
@@ -1,32 +1,56 @@
 #!/usr/bin/env bash
+# shell script to convert date to another timezone
+# usage: dated.sh [date]
 
-# simple oneliner to convert date in various time zones
+# the script will load all the timezones with the following command
+# timedatectl list-timezones
+# the list of timezones is then parsed to group them by continent
+# so the user can interactively select the continent and timezone
 
-set -e  # fails on error
+if [ -z "$1" ]; then
+    utc_date=$(date +"%Y-%m-%d %T %Z")
+else
+    utc_date=$(date -d "$1" +"%Y-%m-%d %T %Z")
+fi
 
-utc_date=$(date -d "$1" +"%Y-%m-%d %T %Z")
+continent_list=`timedatectl list-timezones | cut -f 1 -d / | sort | uniq`
 
-TZs=(
-  'America/Los_Angeles'
-  'America/New_York'
-  'Asia/Tokyo'
-  'Australia/Sydney'
-  'Europe/London'
-  'Europe/Dublin'
-  'Europe/Moscow'
-)
-
-
-printf "+---------------------------------------------------------+ \n"
-printf "Universal Coordinated time: %s \n" \
-"$(date -d "$utc_date" +"%Y-%m-%d %T %Z" -u)"
-printf "+---------------------------------------------------------+ \n"
-
-printf "European Time: \t %s \n" "$(date -d "$utc_date" +"%Y-%m-%d %T %Z" )"
-
-for TZ in "${TZs[@]}"
+# add to the list of continents the 'exit' option
+continent_list="$continent_list exit"
+# show the list of the continents to select from
+select continent in $continent_list
 do
-  loc=$(echo "$TZ" | cut -f 2 -d / | tr '_' ' ')
-  export TZ=$TZ ; printf "$loc:%*s$(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n" \
-  "$((16-${#loc}))"
+    # if the user selected 'exit' then exit the script
+    if [ "$continent" = "exit" ]; then
+        exit
+    fi
+    # if the user selects a continent, then show the list of timezones
+    # in that continent
+    if [ -n "$continent" ]; then
+        timezone_list=`timedatectl list-timezones | grep $continent | cut -f 2 -d / | sort`
+        # add to the list of timezones the 'exit' option
+        timezone_list="BACK $timezone_list"
+        # show the list of timezones to select from
+        select city in $timezone_list; do
+            # if the user selected 'back' then go back to selecting the continent
+            if [ "$city" = "BACK" ]; then
+                continue
+            fi
+            # if the user selects a city, then convert the date
+            # to that timezone
+            if [ -n "$city" ]; then
+                TZ=$continent/$city
+                loc=$(echo "$city" | cut -f 2 -d / | tr '_' ' ')
+                printf "+---------------------------------------------------------+ \n"
+                printf "Universal Coordinated time: %s \n" \
+                "$(date -d "$utc_date" +"%Y-%m-%d %T %Z" -u)"
+                printf "+---------------------------------------------------------+ \n"
+
+                printf "European Time: \t %s \n" "$(date -d "$utc_date" +"%Y-%m-%d %T %Z" )"
+                export TZ=$TZ; printf "$loc:%*s$(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n" \
+                "$((16-${#loc}))"
+                exit
+            fi
+        done
+    fi
 done
