77aecfbbae91f4856b1189ec020b2535b9897eff
kappapiana <carlo@piana.eu>
2022-10-24 11:07:13 +0200

    restore old version, for quick translation anyway

---
+++ dated.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# simple oneliner to convert date in various time zones
+
+set -e  # fails on error
+
+utc_date=$(date -d "$1" +"%Y-%m-%d %T %Z")
+
+
+printf "+---------------------------------------------------------+ \n"
+printf "Universal Coordinated time: $(date -d "$utc_date" +"%Y-%m-%d %T %Z" -u) \n"
+printf "+---------------------------------------------------------+ \n"
+
+printf "European Time: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='America/Los_Angeles'
+printf "Los Angeles: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n"
+
+export TZ='America/New_York'
+printf "New York: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z") \n"
+
+export TZ='Asia/Tokyo'
+printf "Tokyo: \t\t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='Australia/Sydney'
+printf "Sydney: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='Europe/London'
+printf "London: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='Europe/Dublin'
+printf "Dublin: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
+
+export TZ='Europe/Moscow'
+printf "Moscow: \t $(date -d "$utc_date" +"%Y-%m-%d %T %Z" ) \n"
