744ea8cf649e453f372c3e4b8cefbc268ad206e0
kappapiana <carlo@piana.eu>
2022-10-24 11:53:00 +0200

    Add timezone to be passed as variable

--- dated/dated_quick.sh
+++ dated/dated_quick.sh
@@ -2,9 +2,29 @@
 
 # simple oneliner to convert date in various time zones
 
+echo  ""
+echo "usage: insert the date in the STRING format for date
+if you want to enter a different timezone to convert FROM
+just add it as a second variable.
+
+Encapsulate variables within quotation marks
+
+example: ./dated_quick.sh "2022-11-04 11:29" "America/New_York""
+echo  ""
+
 set -e  # fails on error
 
-utc_date=$(date -d "$1" +"%Y-%m-%d %T %Z")
+if [ -n "$2" ]; then
+  echo "piena"
+  input_date="TZ=\"$2\" $1"
+  # echo "$1"
+else
+  input_date="$1"
+  echo "vuota"
+fi
+
+
+utc_date=$(date -d "$input_date" +"%Y-%m-%d %T %Z")
 
 
 printf "+---------------------------------------------------------+ \n"
