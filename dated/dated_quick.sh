df16fca85c21ab0204e2cf408b19145a38f5adc8
kappapiana <carlo@piana.eu>
2022-10-26 15:36:52 +0200

    add option to input from within the script

--- dated/dated_quick.sh
+++ dated/dated_quick.sh
@@ -2,27 +2,60 @@
 
 # simple oneliner to convert date in various time zones
 
-echo  ""
-echo "usage: insert the date in the STRING format for date
+printf "+---------------------------------------------------------+ \n"
+
+echo """
+usage: insert the date in the STRING format for date
 if you want to enter a different timezone to convert FROM
 just add it as a second variable.
 
-Encapsulate variables within quotation marks
+Encapsulate variables within quotation marks.
 
-example: ./dated_quick.sh "2022-11-04 11:29" "America/New_York""
-echo  ""
+example: ./dated_quick.sh "2022-11-04 11:29" "America/New_York"
+
+Otherwise, just leave entry blank and you will be asked
+
+"""
+printf "+---------------------------------------------------------+ \n"
 
 set -e  # fails on error
 
-if [ -n "$2" ]; then
-  echo "piena"
-  input_date="TZ=\"$2\" $1"
-  # echo "$1"
-else
-  input_date="$1"
-  echo "vuota"
+# If nothing is entered in the string, ask user
+
+if [ -z "$1"]; then
+
+  echo "enter date: "
+  read date
+
+  echo "enter timezone (leave blank for current one)"
+  read TZ
+
+else # User has entered at least one string, just use what user passed
+
+  date="$1"
+
+  if [ -n "$2" ]; then
+    # echo "piena"
+    TZ="$2"
+    # echo "$1"
+  else
+    echo "using system timezone"
+  fi
+
 fi
 
+# Now we have both values (including, "no value")
+
+if [ -n "$TZ" ]; then
+
+  input_date="TZ=\"$TZ\" $date"
+  # echo "$1"
+else
+  input_date="$date"
+  # echo "vuota"
+fi
+
+# UTC is always the basis for calculation
 
 utc_date=$(date -d "$input_date" +"%Y-%m-%d %T %Z")
 
